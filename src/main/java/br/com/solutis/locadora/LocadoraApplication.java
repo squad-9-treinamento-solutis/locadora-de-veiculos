a2fc75de57ccce5c9754d2cb869be0c050bb68ac
silvaalejesus <silvaalejesus@gmail.com>
2023-08-15 01:07:58 -0300

    feat: coadicionando dependecias de configuração do projeto

---
+++ src/main/java/br/com/solutis/locadora/LocadoraApplication.java
@@ -0,0 +1,15 @@
+package br.com.solutis.locadora;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class LocadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoraApplication.class, args);
+	}
+
+}
