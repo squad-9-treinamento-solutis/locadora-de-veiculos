a47f18ea47166bb97709f510b47a82c2ea61541b
Victor Kayk <victorkayk77@gmail.com>
2023-08-17 11:04:34 -0300

    refactor: removendo o AbstractController

--- src/main/java/br/com/solutis/locadora/controller/InsurancePolicyController.java
+++ src/main/java/br/com/solutis/locadora/controller/InsurancePolicyController.java
@@ -1,7 +1,5 @@
 package br.com.solutis.locadora.controller;
 
-import br.com.solutis.locadora.model.dto.InsurancePolicyDto;
-import br.com.solutis.locadora.service.CrudService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "InsurancePolicyController")
 @RestController
 @RequestMapping("/insurances")
-public class InsurancePolicyController extends AbstractController<InsurancePolicyDto>{
-
-    public InsurancePolicyController(CrudService<InsurancePolicyDto> service) {
-        super(service);
-    }
+public class InsurancePolicyController {
 
 }
