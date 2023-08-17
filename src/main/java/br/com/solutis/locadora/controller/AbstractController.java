cf7a7e33976aa4dbb1db520c1c81c2129e102572
Victor Kayk <victorkayk77@gmail.com>
2023-08-17 09:58:46 -0300

    feat: criado, editando, listando e apagando InsurancePolicy

--- src/main/java/br/com/solutis/locadora/controller/AbstractController.java
+++ src/main/java/br/com/solutis/locadora/controller/AbstractController.java
@@ -4,7 +4,6 @@ import br.com.solutis.locadora.model.dto.AbstractDto;
 import br.com.solutis.locadora.service.AbstractService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/br/com/solutis/locadora/mapper/GenericMapper.java
+++ src/main/java/br/com/solutis/locadora/mapper/GenericMapper.java
@@ -4,10 +4,15 @@ package br.com.solutis.locadora.mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
+import java.util.List;
+
 public interface GenericMapper<E,D> {
     @Mapping(target = ".", source = ".", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     D modelToDTO(E entity);
 
     @Mapping(target = ".", source = ".", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     E dtoToModel(D dto);
+
+    @Mapping(target = ".", source = ".", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    List<D> listModelToListDto(List<E> all);
 }
