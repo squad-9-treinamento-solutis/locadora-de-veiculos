434e0374a9eb3da75e57e032d44a413064862579
ricardovac <ricardoviicius@gmail.com>
2023-08-17 20:04:34 -0300

    fix: mapper

--- src/main/java/br/com/solutis/locadora/controller/car/CarController.java
+++ src/main/java/br/com/solutis/locadora/controller/car/CarController.java
@@ -1,11 +1,15 @@
 package br.com.solutis.locadora.controller.car;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "CarController")
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/cars")
+@CrossOrigin
 public class CarController {
 }
--- src/main/java/br/com/solutis/locadora/service/car/CarService.java
+++ src/main/java/br/com/solutis/locadora/service/car/CarService.java
@@ -1,7 +1,7 @@
 package br.com.solutis.locadora.service.car;
 
 import br.com.solutis.locadora.exception.BadRequestException;
-import br.com.solutis.locadora.mapper.CarMapperImpl;
+import br.com.solutis.locadora.mapper.car.CarMapper;
 import br.com.solutis.locadora.model.dto.car.CarDto;
 import br.com.solutis.locadora.model.entity.car.Car;
 import br.com.solutis.locadora.repository.CrudRepository;
@@ -21,7 +21,7 @@ import java.util.List;
 @Transactional(propagation = Propagation.REQUIRED)
 public class CarService implements CrudService<CarDto> {
     private final CrudRepository<Car> carRepository;
-    private final CarMapperImpl carMapper;
+    private final CarMapper carMapper;
 
     @Override
     public CarDto findById(Long id) {
@@ -31,6 +31,7 @@ public class CarService implements CrudService<CarDto> {
 
     @Override
     public List<CarDto> findAll(int pageNo, int pageSize) {
+        // Verificar a data
         Pageable paging = PageRequest.of(pageNo, pageSize);
         Page<Car> cars = carRepository.findAll(paging);
 
