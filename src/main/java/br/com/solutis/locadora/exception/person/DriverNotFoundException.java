640ff09ba636d941036d5120f603897e3d6a8e2c
Candeias-Vinicius <vinicius.candeias@ucsal.edu.br>
2023-08-18 14:48:29 -0300

    Finalizado crud de motorista

---
+++ src/main/java/br/com/solutis/locadora/exception/person/DriverNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.solutis.locadora.exception.person;
+
+public class DriverNotFoundException extends RuntimeException{
+    public DriverNotFoundException(Long id) {
+        super("Driver with ID " + id + " not found.");
+    }
+}
