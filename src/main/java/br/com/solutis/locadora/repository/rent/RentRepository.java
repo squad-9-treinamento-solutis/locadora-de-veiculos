f9ba8757cc6b92d98bf2591c58b806833db90d4c
silvaalejesus <silvaalejesus@gmail.com>
2023-08-22 13:40:42 -0300

    feat: listando alugueis ativos

--- src/main/java/br/com/solutis/locadora/repository/rent/RentRepository.java
+++ src/main/java/br/com/solutis/locadora/repository/rent/RentRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface RentRepository extends JpaRepository<Rent, Long> {
     List<Rent> findByFinishedTrue();
+    List<Rent> findByFinishedFalse();
 }
